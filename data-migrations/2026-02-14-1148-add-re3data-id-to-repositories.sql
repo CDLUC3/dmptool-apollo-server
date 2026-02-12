7a9fefeaaae558bff91738c8353225da9a9ef20f
Scott F <Scott.Fisher@ucop.edu>
2026-02-12 10:55:31 -0800

    update re3data id in migration

--- data-migrations/2026-02-14-1148-add-re3data-id-to-repositories.sql
+++ data-migrations/2026-02-14-1148-add-re3data-id-to-repositories.sql
@@ -1,2 +1,11 @@
 ALTER TABLE `repositories` ADD COLUMN `re3data_id` varchar(255) DEFAULT NULL;
 ALTER TABLE `repositories` ADD UNIQUE KEY `unique_re3data_id` (`re3data_id`);
+
+UPDATE repositories
+SET re3data_id = SUBSTRING(
+  uri,
+  LENGTH('https://www.re3data.org/repository/') + 1
+                 )
+WHERE uri LIKE 'https://www.re3data.org/repository/%'
+  AND (re3data_id IS NULL OR re3data_id = '');
+
